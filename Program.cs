337167c4cf25044fc125fec22dc1c4f032412332
Siddhant <santoshshrivastava39@gmail.com>
2022-03-08 01:15:30 +0530

    Call in main method

--- Program.cs
+++ Program.cs
@@ -9,30 +9,9 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Number of Unique Coupons you want to generate-");
-            int n = Convert.ToInt32(Console.ReadLine());
-        
-            int totalRandomNo = 0;
-            Random random = new Random();
-            while (totalRandomNo < n)
-            {
-                int coupon = random.Next(1, 1000);
-                Console.WriteLine(coupon);
-                totalRandomNo++;
-            }
-            Console.WriteLine("The no of times random used in generating unique coupons is :" + totalRandomNo);
-
-
-
-
-
-
-
-
-
-
-
+            Reversenumber.revnumber();
         }
+           
 
     }
 
