d18b0342a90ff8570199a1a5f8b5d05463b299a6
Siddhant <santoshshrivastava39@gmail.com>
2022-03-08 01:12:22 +0530

    update the main method

--- Program.cs
+++ Program.cs
@@ -9,12 +9,35 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            Reversenumber.reverse_number();
+            Console.WriteLine("Enter the Number of Unique Coupons you want to generate-");
+            int n = Convert.ToInt32(Console.ReadLine());
+        
+            int totalRandomNo = 0;
+            Random random = new Random();
+            while (totalRandomNo < n)
+            {
+                int coupon = random.Next(1, 1000);
+                Console.WriteLine(coupon);
+                totalRandomNo++;
+            }
+            Console.WriteLine("The no of times random used in generating unique coupons is :" + totalRandomNo);
+
+
+
+
+
+
+
+
+
+
 
         }
 
-
-
-
     }
+
+
+    
+
+
 }
