b58b6767f9877a7c3e179ab5179614c91f5ff5d4
Siddhant <santoshshrivastava39@gmail.com>
2022-03-07 23:55:52 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programs");
+
+        }
+
+
+
+
+    }
+}
