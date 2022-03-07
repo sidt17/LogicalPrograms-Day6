be929a21385a4198de8174c6270bf2e993c44267
Siddhant <santoshshrivastava39@gmail.com>
2022-03-08 01:32:10 +0530

    Coupan Numbers

---
+++ CoupanNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    public class CoupanNumber
+    {
+     public static void Generatecoupanno()
+        {
+            Console.WriteLine("Enter the Number of Unique Coupons :");
+            int n = Convert.ToInt32(Console.ReadLine());
+       
+            int totalRandomNo = 0;
+            Random random = new Random();
+            while (totalRandomNo < n)
+            {
+                int coupon = random.Next(1, 2000);
+                Console.WriteLine(coupon);
+
+                totalRandomNo++;
+            }
+            Console.WriteLine("The no of times random used in generating unique coupons is " + totalRandomNo);
+
+
+
+        }
+
+    }
+         
+    
+}
